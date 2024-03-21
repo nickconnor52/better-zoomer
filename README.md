1134518ada805fc076b4618947976f0bb5a94fbb
nconnor1 <nconnor@ontra.ai>
2024-03-21 11:14:56 -0400

    Support array & add README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Description
+
+A basic bash script that accepts a URL or CSV list of URLs to attempt to download via dezoomify-rs ([docs here](https://github.com/lovasoa/dezoomify-rs))
+
+## Usage
+
+
+## Troubleshooting
+
+It's possible (likely, even) that you'll need `dezoomify-rs` for this to work. The following should unblock you:
+
+```bash
+brew install dezoomify-rs
+```
--- get-by-url.sh
+++ get-by-url.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 -u <URL>"
+    echo "Usage: $0 -u <URL>, -f <RELATIVE_CSV_LOCATION>"
+    echo "  -u: URL of the image to download"
+    echo "  -f: relative location of the CSV file containing URLs to be downloaded, one URL per line"
     exit 1
 }
 
-# Parse command line options
-while getopts ":u:" opt; do
-    case $opt in
-        u)
-            url="$OPTARG"
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            usage
-            ;;
-        :)
-            echo "Option -$OPTARG requires an argument." >&2
-            usage
-            ;;
-    esac
+while getopts 'u:f:' flag; do
+  case "${flag}" in
+    u) url="${OPTARG}" ;;
+    f) file="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
 done
 
-# Check if URL is provided
-if [ -z "$url" ]; then
-    echo "URL is required."
+
+# # Check if URL is provided
+if [ -z "$url" ] && [ -z "$file" ]; then
+    echo "URL or File location is required."
     usage
 fi
 
@@ -37,32 +32,52 @@ is_integer() {
     fi
 }
 
-response="$(expect -c "
-  spawn dezoomify-rs {$url}
-  expect {Which level do you want to download?}
-  exit 1
-")"
+download_image_by_url() {
+  url="$1"
 
-highest_number=0
+  response="$(expect -c "
+    spawn dezoomify-rs {$url}
+    expect {Which level do you want to download?}
+    exit 1
+  ")"
 
-while read -r line; do
-  number=$(echo "$line" | grep -o '^[0-9]\+')
-  valid_number= is_integer "$number"
+  highest_number=0
 
-  if [ $is_integer "$number" ]; then
-      highest_number="$number"
+  while read -r line; do
+    number=$(echo "$line" | grep -o '^[0-9]\+')
+    valid_number= is_integer "$number"
+
+    if [ $is_integer "$number" ]; then
+        highest_number="$number"
+    fi
+  done <<< "$response"
+
+  if [ $highest_number -eq 0 ]; then
+    echo "No valid number found"
+    exit 1
   fi
-done <<< "$response"
 
-if [ $highest_number -eq 0 ]; then
-  echo "No valid number found"
-  exit 1
+  /usr/bin/expect -c "
+    spawn dezoomify-rs {$url}
+    expect -re {Which level do you want to download?}
+    send -- "$highest_number\\r"
+    expect -re {Image successfully saved to}
+    exit 1
+  "
+
+  return 0
+}
+
+urls=()
+
+if [ -n "$url" ]; then
+  urls+=("$url")
+elif [ -n "$file" ]; then
+  while read -r line; do
+    urls+=("$line")
+  done < "$file"
 fi
 
-/usr/bin/expect -c "
-  spawn dezoomify-rs {$url}
-  expect -re {Which level do you want to download?}
-  send -- "$highest_number\\r"
-  expect -re {Image successfully saved to}
-  exit 1
-"
+for line in "${urls[@]}"; do
+  download_image_by_url "$line"
+done
